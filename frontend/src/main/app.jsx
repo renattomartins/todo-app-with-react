562603c15bfad4b9bb1ccee1018b4d7ae2ea1b17
Renato Martins <renatto.martins@gmail.com>
2018-04-23 08:15:11 -0300

    Add a menu component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,9 +4,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
     <div className='containter'>
+        <Menu />
         <Todo />
         <About />
     </div>
