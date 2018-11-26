6a458651e12ecc82c7938db0eda73d251ac1a22d
Renato Martins <renatto.martins@gmail.com>
2018-11-26 12:01:53 -0200

    Converts TodoForm functional component to class based component

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
@@ -6,8 +6,13 @@ import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
-    const keyHandler = (e) => {
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    keyHandler(e) {
         if (e.key === 'Enter') {
             e.shiftKey ? props.handleSearch() : props.handleAdd()
         } else if (e.key === 'Escape') {
@@ -15,30 +20,32 @@ const TodoForm = props => {
         }
     }
 
-    return (
-        <div role='form' className='todoForm'>
-            <Grid cols='12 9 10'>
-                <input id='description'
-                    className='form-control'
-                    placeholder='Adicione uma tarefa'
-                    onChange={props.changeDescription}
-                    onKeyUp={keyHandler}
-                    value={props.description}></input>
-            </Grid>
-    
-            <Grid cols='12 3 2'>
-                <IconButton style='primary'
-                    icon='plus'
-                    onClick={props.handleAdd}></IconButton>
-                <IconButton style='info'
-                    icon='search'
-                    onClick={props.handleSearch}></IconButton>
-                <IconButton style='default'
-                    icon='close'
-                    onClick={props.handleClear}></IconButton>
-            </Grid>
-        </div>
-    )
+    render() {
+        return (
+            <div role='form' className='todoForm'>
+                <Grid cols='12 9 10'>
+                    <input id='description'
+                        className='form-control'
+                        placeholder='Adicione uma tarefa'
+                        onChange={this.props.changeDescription}
+                        onKeyUp={this.keyHandler}
+                        value={this.props.description}></input>
+                </Grid>
+
+                <Grid cols='12 3 2'>
+                    <IconButton style='primary'
+                        icon='plus'
+                        onClick={this.props.handleAdd}></IconButton>
+                    <IconButton style='info'
+                        icon='search'
+                        onClick={this.props.handleSearch}></IconButton>
+                    <IconButton style='default'
+                        icon='close'
+                        onClick={this.props.handleClear}></IconButton>
+                </Grid>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
